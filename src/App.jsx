f4e34a1c8f033e7b3ec0e95d815149da076335d3
Moss Tang <V002651@aver.com>
2026-09-03 15:23:24 +0800

    Initial TR615 prototype

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import TR615PaintLook from './TR615PaintLookV6'
+
+function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden' }}>
+      <TR615PaintLook />
+    </div>
+  )
+}
+
+export default App
