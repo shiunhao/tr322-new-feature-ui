7f06257727c5382ec3c8e03f9e889f64af26b218
Moss Tang <V002651@aver.com>
2026-09-07 10:28:16 +0800

    Order priority modes by resolution

--- src/TR322PaintLookV6.jsx
+++ src/TR322PaintLookV6.jsx
@@ -1120,7 +1120,7 @@ function TR322VideoAudioPage({ settings, onChange }) {
             <Select val={settings.powerFreq} options={["50Hz", "59.94Hz", "60Hz"]} onChange={(value) => onChange("powerFreq", value)} style={selectStyle} />
           </FormField>
           <FormField label="Priority Mode" style={fieldStyle}>
-            <Select val={settings.priorityMode} options={licenseActive ? ["2K60", "4K30"] : ["2K60"]} onChange={(value) => onChange("priorityMode", value)} style={selectStyle} />
+            <Select val={settings.priorityMode} options={licenseActive ? ["4K30", "2K60"] : ["2K60"]} onChange={(value) => onChange("priorityMode", value)} style={selectStyle} />
           </FormField>
           <FormField label="PIP/PBP Mode" style={fieldStyle}>
             <Select val={settings.pipPbpMode} options={["PIP(PTZ/Wide) Small"]} onChange={(value) => onChange("pipPbpMode", value)} style={selectStyle} />
